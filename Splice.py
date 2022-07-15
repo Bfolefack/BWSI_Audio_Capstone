8fa2d17d01454b9f780012d74ca55f5cd775ac09
Boueny Folefack <bouenyfolefack@gmail.com>
2022-07-15 12:10:39 -0500

    Synchronized disparate .py files to work together

--- Splice.py
+++ Splice.py
@@ -3,12 +3,12 @@ from IPython.display import Audio
 import librosa
 import random
 
-def random_splice(song_path, snippets, duration):
+def random_splice(song_path, snippets):
     samples, sampling_rate = librosa.load(song_path, sr=44100, mono=True, duration=180)
     length = len(samples) #2138112
     clips = []
     for i in range(snippets):
-        end = duration * 44100 #1323000
-        factor = random.randint(0, (length - end))
-        clips.append(samples[(0+factor):(end+factor)])
+        start = random.randint(0, length - 2)
+        end = start + random.randint(start, length)
+        clips.append(samples[start:end])
     return clips
